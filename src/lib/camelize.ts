9a36cbc8ee21c63d217c93b404021254b4f7d1f3
dzucconi <mail@damonzucconi.com>
2020-05-31 13:46:14 -0400

    Fixes camelize

--- src/lib/camelize.ts
+++ src/lib/camelize.ts
@@ -1,7 +1,6 @@
 export const camelize = (string: string) => {
-  string = string.replace(/[-_\s.]+(.)?/g, (_, c) =>
-    c ? c.toUpperCase() : ""
-  );
-
+  string = string
+    // Remove any non-word chars and upcase the next word char if present
+    .replace(/[\W_]+(\w)?/g, (_, c) => (c ? c.toUpperCase() : ""));
   return string.substr(0, 1).toLowerCase() + string.substr(1);
 };
